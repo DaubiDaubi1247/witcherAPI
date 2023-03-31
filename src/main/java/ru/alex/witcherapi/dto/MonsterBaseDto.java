d09ed3d0fa57ec3ffb574875dd58b6c56de1070a
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-03-31 11:57:43 +0300

    refactor MonsterBaseDto

--- src/main/java/ru/alex/witcherapi/dto/MonsterBaseDto.java
+++ src/main/java/ru/alex/witcherapi/dto/MonsterBaseDto.java
@@ -1,13 +1,23 @@
 package ru.alex.witcherapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import ru.alex.witcherapi.entity.MonsterBase;
 
 @SuperBuilder
 @Getter
 @Setter
-public class MonsterBaseDto extends MonsterBase {
+public class MonsterBaseDto {
+
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String imgName;
+
+    private String source;
 
 }
